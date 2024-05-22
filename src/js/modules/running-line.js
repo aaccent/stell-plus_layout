cbdf1613b0c9979788370836727a16c2d9e2f65b
Regina <rmyachina@mail.ru>
2024-05-22 14:56:01 +0300

    fix: issue if width of running line changed after load page

--- src/js/modules/running-line.js
+++ src/js/modules/running-line.js
@@ -2,19 +2,26 @@ function getWrapperItems(wrapper) {
   return wrapper.querySelectorAll('.running-line__item')
 }
 
-function setAnimationVariables(wrapper) {
-  const durationInMs = wrapper.offsetWidth / .18
+function setAnimationVariables(wrapper, wrapperWidth) {
+  const firstItemWidth = wrapper.querySelector('.running-line__item')?.offsetWidth
+  const width = firstItemWidth > wrapperWidth ? firstItemWidth : wrapperWidth
+  const durationInMs = Math.trunc(width / .18)
 
   wrapper.style.setProperty('--animation-duration', `${durationInMs}ms`)
-  wrapper.style.setProperty('--animation-translate', `-${wrapper.offsetWidth}px`)
+  wrapper.style.setProperty('--animation-translate', `-${width}px`)
 }
 
 
-export function initRunningLine(wrapper) {
-  setAnimationVariables(wrapper)
+function initRunningLine(wrapper) {
+  const initWidth = wrapper.offsetWidth
+  setAnimationVariables(wrapper, initWidth)
+
+  const resizeObserver = new ResizeObserver(() => setAnimationVariables(wrapper, initWidth))
 
   let items = getWrapperItems(wrapper)
   const itemWidth = items[0].offsetWidth
+
+  items.forEach((item) => resizeObserver.observe(item))
 
   let itemsCopied = 1
 
