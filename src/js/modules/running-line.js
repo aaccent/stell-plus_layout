630bcbbc5d88fea12b92cbae3139d37b6eda346d
Regina <rmyachina@mail.ru>
2024-05-22 14:16:22 +0300

    refactor: rewrite animation of running line

--- src/js/modules/running-line.js
+++ src/js/modules/running-line.js
@@ -2,7 +2,17 @@ function getWrapperItems(wrapper) {
   return wrapper.querySelectorAll('.running-line__item')
 }
 
+function setAnimationVariables(wrapper) {
+  const durationInMs = wrapper.offsetWidth / .18
+
+  wrapper.style.setProperty('--animation-duration', `${durationInMs}ms`)
+  wrapper.style.setProperty('--animation-translate', `-${wrapper.offsetWidth}px`)
+}
+
+
 export function initRunningLine(wrapper) {
+  setAnimationVariables(wrapper)
+
   let items = getWrapperItems(wrapper)
   const itemWidth = items[0].offsetWidth
 
